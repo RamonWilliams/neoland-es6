72e850fd40163f2bc28f7217541531c4d4b0eaf4
Ramon Williams <ramnwor21@gmail.com>
2022-07-25 15:35:17 +0200

    Iteracion 2.4

--- app.js
+++ app.js
@@ -59,3 +59,27 @@ const fruits = ['Banana', 'Strawberry', 'Orange'];
 const [fruta1,fruta2,fruta3] = fruits;
 console.log(fruta1);
 
+/*2.3 En base al siguiente javascript, usa destructuring para crear 2 
+variables igualandolo a la función e imprimiendolo por consola.
+
+const animalFunction = () => {
+    return {name: 'Bengal Tiger', race: 'Tiger'}
+};*/
+
+
+
+/*2.4 En base al siguiente javascript, usa destructuring para crear las 
+variables name y itv con sus respectivos valores. Posteriormente crea 
+3 variables usando igualmente el destructuring para cada uno de los años 
+y comprueba que todo esta bien imprimiendolo.*/
+
+const car = {name: 'Mazda 6', itv: [2015, 2011, 2020] };
+
+const{name,itv} = car;
+console.log(name);
+console.log(itv);
+
+const[itv1,itv2,itv3] = itv;
+console.log(itv1);
+
+
