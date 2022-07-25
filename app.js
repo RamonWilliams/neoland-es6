7a5eec3fd904ab567396c472d7b2b339a2ef87f7
Ramon Williams <ramnwor21@gmail.com>
2022-07-25 15:37:10 +0200

    Iteracion 3.2

--- app.js
+++ app.js
@@ -90,3 +90,11 @@ const pointsList = [32, 54, 21, 64, 75, 43];
  const copyPointList = [...pointsList];
 
 
+console.log(copyPointList);
+
+//3.2 Dado el siguiente objeto, crea una copia usando spread operators.
+const toy = {name: 'Bus laiyiar', date: '20-30-1995', color: 'multicolor'};
+
+const copyToy = {...toy};
+console.log(copyToy);
+
