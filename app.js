76de6741d6bd183f9999c99569379ab77d590191
Ramon Williams <ramnwor21@gmail.com>
2022-07-25 15:34:06 +0200

    Iteracion 2.2

--- app.js
+++ app.js
@@ -50,4 +50,12 @@ console.log(title)
 console.log(year)
 
 
+/*2.2 En base al siguiente javascript, usa destructuring para crear 3 variables 
+llamadas fruit1, fruit2 y fruit3, con los valores del array. Posteriormente
+imprimelo por consola.*/
+
+const fruits = ['Banana', 'Strawberry', 'Orange'];
+
+const [fruta1,fruta2,fruta3] = fruits;
+console.log(fruta1);
 
